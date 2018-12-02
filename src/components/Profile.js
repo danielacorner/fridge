d856eea7f4f91e48f810d942d43ed8f3e58137c5
Eric Liu <eliu1991@gmail.com>
2018-12-01 20:44:59 -0500

    added log out

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import { navigate } from "@reach/router";
 
-export default class Profile extends Component {
+import { withFirebase } from "../services/Firebase";
+import { PATHS } from "../const/paths";
+
+class Profile extends Component {
+  handleSignOut = e => {
+    this.props.firebase.userSignOut();
+    navigate(PATHS.LANDING);
+  };
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <Button color="primary" onClick={this.handleSignOut}>
+          Sign Out
+        </Button>
+      </div>
+    );
   }
 }
+
+export default withFirebase(Profile);
