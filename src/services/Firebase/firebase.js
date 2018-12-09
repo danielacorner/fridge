21e4ef823cb25accd9d76db0e8e6574f7a8747d5
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2018-12-09 10:43:35 -0500

    initial work

--- src/services/Firebase/firebase.js
+++ src/services/Firebase/firebase.js
@@ -1,6 +1,16 @@
 import firebase from 'firebase';
 import 'firebase/auth';
-import config from './firebase.config.js';
+import config from './firebase.config.js'
+const PATHS = {
+  FOOD_LIST: '/foodList'
+};
+
+const uuidv4 = () => {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  )
+}
+
 class Firebase {
   constructor() {
     this.app = firebase.initializeApp( config );
@@ -27,7 +37,31 @@ class Firebase {
 
   userPasswordReset = email => this.auth.sendPasswordResetEmail( email );
 
-  userPasswordUpdate = password => this.auth.currentUser.updatePassword( password );
+  userPasswordUpdate = password =>  this.auth.currentUser.updatePassword(password);
+
+  async createFoodItem (uid, { name, category, isPurchased, datePurchased }) {
+    const foodList = await firebase.database().ref(`${PATHS.FOOD_LIST}/${uid}`).once('value')
+    const itemId = uuidv4();
+    const itemData = { name, category, isPurchased, datePurchased }
+    if (!foodList.val().length) {
+      firebase.database().ref('foodList/' + uid).set({
+          [itemId]: itemData
+      });
+      return true;
+    }
+    // fridgeItems[itemId] = {
+    //   name: fridgeNames[index],
+    //   category: fridgeCategories[index],
+    //   dateAdded: randomDate(new Date(Date.parse(users[uid].dateCreated)), new Date()).toString(),
+    //   isPurchased: !!Math.floor(Math.random() * 2)
+
+    // }
+
+    // firebase.database().ref('foodList/' + foodListId).set({
+    //   userId: uid,
+    //   items: fridgeItems
+    // });
+  }
 }
 
 export default Firebase;
